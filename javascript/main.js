d3efb7bc13da4a0ea6d0f3e6d5cc436de9dace5c
Max Jendruk <max.jendruk@student.hpi.uni-potsdam.de>
2015-07-24 00:31:09 +0200

    add scroll to top button

--- javascript/main.js
+++ javascript/main.js
@@ -3,5 +3,20 @@
   $(document).on('click','.js-select-all', function() { 
     this.select(); 
   });
+
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 400) {
+        $('.scroll-to-top-button').fadeIn();
+    } else {
+        $('.scroll-to-top-button').fadeOut();
+    }
+  });
+
+  $('.scroll-to-top-button').click(function () {
+    $("html, body").animate({
+        scrollTop: 0
+    }, 300);
+    return false;
+  });
     
 });
